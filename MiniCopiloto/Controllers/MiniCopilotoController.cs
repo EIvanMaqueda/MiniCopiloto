06889444e2074122d27a5ebff9a7dd946a63f988
EIvanMaqueda <eivanmaqueda@gmail.com>
2023-05-09 15:29:26 -0600

    Add project files.

---
+++ MiniCopiloto/Controllers/MiniCopilotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniCopiloto.Controllers
+{
+    public class MiniCopilotoController : Controller
+    {
+        // GET: MiniCopiloto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
